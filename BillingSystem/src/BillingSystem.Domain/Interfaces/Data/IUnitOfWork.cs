acc6cf56fd9bfa84f2dc9183bfa5fee97cd6a5c1
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-25 12:46:43 -0300

    FEAT: Adding unit of work

---
+++ BillingSystem/src/BillingSystem.Domain/Interfaces/Data/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace BillingSystem.Domain.Interfaces.Data
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ BillingSystem/src/BillingSystem.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using BillingSystem.Domain.Interfaces.Data;
+
+namespace BillingSystem.Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BillingSystemDbContext _context;
+
+        public UnitOfWork(BillingSystemDbContext context) => _context = context;
+
+        public Task CommitAsync(CancellationToken cancellationToken = default)
+        {
+            return _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
