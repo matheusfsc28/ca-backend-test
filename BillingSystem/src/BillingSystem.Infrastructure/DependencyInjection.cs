a2045ec3589833de401cad800780408624820d94
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-25 12:59:23 -0300

    FEAT: Adding dependency injection to infrastructure layer

---
+++ BillingSystem/src/BillingSystem.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using BillingSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BillingSystem.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddDbContext(services, configuration);
+
+            return services;
+        }
+
+        private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("ConnectionStrings:PostgresqlConnection");
+
+            services.AddDbContext<BillingSystemDbContext>(opt =>
+            {
+                opt.UseNpgsql(connectionString);
+                opt.UseSnakeCaseNamingConvention();
+            });
+        }
+    }
+}
