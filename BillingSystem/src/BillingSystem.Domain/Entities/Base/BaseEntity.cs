11ea26f8ddad2161bee7326174af7c8da09d8ed3
Matheus Felipe <matheus.felipescruz@gmail.com>
2026-02-25 08:53:25 -0300

    FEAT: Adding Base Entity

---
+++ BillingSystem/src/BillingSystem.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace BillingSystem.Domain.Entities.Base
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; protected set; } = DateTime.UtcNow;
+        public DateTime? DeletedAt { get; protected set; }
+        public bool IsDeleted => DeletedAt != null;
+        public void Delete() => DeletedAt = DateTime.UtcNow;
+        public void Restore() => DeletedAt = null;
+        public void Touch() => UpdatedAt = DateTime.UtcNow;
+    }
+}
