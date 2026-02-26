9d2f269d20e7e1889b95c359910b6e23c4c9030c
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 14:49:28 -0300

    FEAT: Adding DeleteCustomerCommandValidator

---
+++ BillingSystem/src/BillingSystem.Application/Commands/Customers/DeleteCustomer/DeleteCustomerCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using BillingSystem.Common.Exceptions;
+using FluentValidation;
+
+namespace BillingSystem.Application.Commands.Customers.DeleteCustomer
+{
+    public class DeleteCustomerCommandValidator : AbstractValidator<DeleteCustomerCommand>
+    {
+        public DeleteCustomerCommandValidator()
+        {
+            RuleFor(request => request.Id)
+                .NotEmpty().WithMessage(ResourceMessagesException.ID_EMPTY);
+        }
+    }
+}
