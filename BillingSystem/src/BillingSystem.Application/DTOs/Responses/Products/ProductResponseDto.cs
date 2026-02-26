bad98ef5f20b8a28cd1b0ced2e27829d7125ff52
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 11:22:02 -0300

    FEAT: Adding product response DTO

---
+++ BillingSystem/src/BillingSystem.Application/DTOs/Responses/Products/ProductResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using BillingSystem.Application.DTOs.Responses.Base;
+using BillingSystem.Domain.Entities.Products;
+
+namespace BillingSystem.Application.DTOs.Responses.Products
+{
+    public class ProductResponseDto : BaseResponseDto
+    {
+        public string Name { get; set; }
+
+        public ProductResponseDto(Product product)
+        {
+            Id = product.Id;
+            Name = product.Name;
+            CreatedAt = product.CreatedAt;
+            UpdatedAt = product.UpdatedAt;
+            DeletedAt = product.DeletedAt;
+        }
+    }
+}
