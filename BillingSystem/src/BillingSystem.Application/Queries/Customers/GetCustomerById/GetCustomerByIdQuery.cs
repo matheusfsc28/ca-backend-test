9adce0e68e4ba541873ff5df9ad58809b1082129
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 18:53:59 -0300

    FEAT: Adding query and handler to get customer by id

---
+++ BillingSystem/src/BillingSystem.Application/Queries/Customers/GetCustomerById/GetCustomerByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using BillingSystem.Application.DTOs.Responses.Customers;
+using MediatR;
+
+namespace BillingSystem.Application.Queries.Customers.GetCustomerById
+{
+    public record GetCustomerByIdQuery(Guid Id) : IRequest<CustomerResponseDto>;
+}
---
+++ BillingSystem/src/BillingSystem.Application/Queries/Customers/GetCustomerById/GetCustomerByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using BillingSystem.Application.DTOs.Responses.Customers;
+using BillingSystem.Common.Exceptions;
+using BillingSystem.Common.Exceptions.BaseExceptions;
+using BillingSystem.Domain.Interfaces.Repositories.Customers;
+using MediatR;
+
+namespace BillingSystem.Application.Queries.Customers.GetCustomerById
+{
+    public class GetCustomerByIdQueryHandler : IRequestHandler<GetCustomerByIdQuery, CustomerResponseDto>
+    {
+        private readonly ICustomerReadRepository _customerReadRepository;
+
+        public GetCustomerByIdQueryHandler(ICustomerReadRepository customerReadRepository)
+        {
+            _customerReadRepository = customerReadRepository;
+        }
+
+        public async Task<CustomerResponseDto> Handle(GetCustomerByIdQuery request, CancellationToken cancellationToken)
+        {
+            var customer = await _customerReadRepository.GetByIdAsync(request.Id, cancellationToken)
+                ?? throw new NotFoundException(ResourceMessagesException.CUSTOMER_NOT_FOUND);
+
+            return new CustomerResponseDto(customer);
+        }
+    }
+}
