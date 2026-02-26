6abebf9f1a79bc19f3297042abd1dcf72ebed7bc
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 01:32:43 -0300

    FEAT: Adding RegisterCustomerCommandBuilder for Validator tests

---
+++ BillingSystem/tests/CommonTestUtilities/Commands/Customers/NovaPasta/RegisterCustomerCommandBuilder.cs
@@ -0,0 +1,19 @@
+﻿using BillingSystem.Application.Commands.Customers.RegisterCustomer;
+using Bogus;
+
+namespace CommonTestUtilities.Commands.Customers.NovaPasta
+{
+    public class RegisterCustomerCommandBuilder
+    {
+        public static RegisterCustomerCommand Build(int nameLength = 10)
+        {
+            return new Faker<RegisterCustomerCommand>()
+                .CustomInstantiator(f => new RegisterCustomerCommand(
+                    Guid.NewGuid(),
+                    f.Person.FullName.PadRight(nameLength, 'a'),
+                    f.Internet.Email(f.Person.FirstName).ToLower(),
+                    f.Address.FullAddress()
+                ));
+        }
+    }
+}
