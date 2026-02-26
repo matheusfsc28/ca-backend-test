9693deff6e9d48685b7d5b4b624a537eab65ec9b
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 11:33:35 -0300

    FIX: Adjusting folder name on RegisterCustomerCommandBuilder

--- BillingSystem/tests/CommonTestUtilities/Commands/Customers/RegisterCustomer/RegisterCustomerCommandBuilder.cs
+++ BillingSystem/tests/CommonTestUtilities/Commands/Customers/RegisterCustomer/RegisterCustomerCommandBuilder.cs
@@ -1,18 +1,18 @@
 ﻿using BillingSystem.Application.Commands.Customers.RegisterCustomer;
 using Bogus;
 
-namespace CommonTestUtilities.Commands.Customers.NovaPasta
+namespace CommonTestUtilities.Commands.Customers.RegisterCustomer
 {
     public class RegisterCustomerCommandBuilder
     {
-        public static RegisterCustomerCommand Build(int nameLength = 10)
+        public static RegisterCustomerCommand Build(int nameLength = 10, int emailLenght = 10, int addressLength = 10)
         {
             return new Faker<RegisterCustomerCommand>()
                 .CustomInstantiator(f => new RegisterCustomerCommand(
                     Guid.NewGuid(),
                     f.Person.FullName.PadRight(nameLength, 'a'),
-                    f.Internet.Email(f.Person.FirstName).ToLower(),
-                    f.Address.FullAddress()
+                    f.Internet.Email(f.Person.FirstName).PadRight(emailLenght, 'a').ToLower(),
+                    f.Address.FullAddress().PadRight(addressLength, 'a')
                 ));
         }
     }
