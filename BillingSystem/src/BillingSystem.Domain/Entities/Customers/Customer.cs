d70d845ea7eaf3c4b5011cbaef00bf42fb3bdb5b
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 01:29:15 -0300

    FEAT: Improving customer entity validations

--- BillingSystem/src/BillingSystem.Domain/Entities/Customers/Customer.cs
+++ BillingSystem/src/BillingSystem.Domain/Entities/Customers/Customer.cs
@@ -38,11 +38,20 @@ namespace BillingSystem.Domain.Entities.Customers
             if (string.IsNullOrEmpty(Name))
                 errors.Add(ResourceMessagesException.NAME_EMPTY);
 
+            if (Name.Length > 150)
+                errors.Add(string.Format(ResourceMessagesException.NAME_MAX_LENGTH, 150));
+
             if (string.IsNullOrEmpty(Email))
                 errors.Add(ResourceMessagesException.EMAIL_EMPTY);
 
+            if (Email.Length > 150)
+                errors.Add(string.Format(ResourceMessagesException.EMAIL_MAX_LENGTH, 150));
+
             if (string.IsNullOrEmpty(Address))
                 errors.Add(ResourceMessagesException.ADDRESS_EMPTY);
+
+            if(Address.Length > 250)
+                errors.Add(string.Format(ResourceMessagesException.ADDRESS_MAX_LENGTH, 250));
 
             if (errors.Count > 0)
                 throw new ErrorOnValidationException(errors);
