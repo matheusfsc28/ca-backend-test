b12cc3a90f01937c994ebc75f5231979296e2c45
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 16:09:47 -0300

    FEAT: Adding UpdateProductCommandValidator

---
+++ BillingSystem/src/BillingSystem.Application/Commands/Products/UpdateProduct/UpdateProductCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using BillingSystem.Common.Exceptions;
+using FluentValidation;
+
+namespace BillingSystem.Application.Commands.Products.UpdateProduct
+{
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(request => request.Id)
+                .NotEmpty().WithMessage(ResourceMessagesException.ID_EMPTY);
+
+            When(request => request.RequestDto.Name != null, () =>
+            {
+                RuleFor(request => request.RequestDto.Name)
+                    .NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY)
+                    .MaximumLength(100).WithMessage(string.Format(ResourceMessagesException.NAME_MAX_LENGTH, 100));
+            });
+        }
+    }
+}
