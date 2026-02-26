07d4344c8242a3ff1e32be59a4237be40570808e
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 19:09:34 -0300

    FIX: Adding query and handle to get product by id

---
+++ BillingSystem/src/BillingSystem.Application/Queries/Products/GetProductById/GetProductByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using BillingSystem.Application.DTOs.Responses.Products;
+using MediatR;
+
+namespace BillingSystem.Application.Queries.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IRequest<ProductResponseDto>;
+}
---
+++ BillingSystem/src/BillingSystem.Application/Queries/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using BillingSystem.Application.DTOs.Responses.Products;
+using BillingSystem.Common.Exceptions;
+using BillingSystem.Common.Exceptions.BaseExceptions;
+using BillingSystem.Domain.Interfaces.Repositories.Products;
+using MediatR;
+
+namespace BillingSystem.Application.Queries.Products.GetProductById
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductResponseDto>
+    {
+        private readonly IProductReadRepository _productReadRepository;
+
+        public GetProductByIdQueryHandler(IProductReadRepository productReadRepository)
+        {
+            _productReadRepository = productReadRepository;
+        }
+
+        public async Task<ProductResponseDto> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _productReadRepository.GetByIdAsync(request.Id, cancellationToken)
+                ?? throw new NotFoundException(ResourceMessagesException.PRODUCT_NOT_FOUND);
+
+            return new ProductResponseDto(product);
+        }
+    }
+}
