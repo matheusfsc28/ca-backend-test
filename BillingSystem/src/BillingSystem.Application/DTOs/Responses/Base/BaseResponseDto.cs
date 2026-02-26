9e1cd60a2ca0993a25fb97af4e957f7bd7caf1eb
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 01:25:51 -0300

    FIX: Adding response when a customer is registered

---
+++ BillingSystem/src/BillingSystem.Application/DTOs/Responses/Base/BaseResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BillingSystem.Application.DTOs.Responses.Base
+{
+    public abstract class BaseResponseDto
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ BillingSystem/src/BillingSystem.Application/DTOs/Responses/Customers/CustomerResponseDto.cs
@@ -0,0 +1,23 @@
+﻿using BillingSystem.Application.DTOs.Responses.Base;
+using BillingSystem.Domain.Entities.Customers;
+
+namespace BillingSystem.Application.DTOs.Responses.Customers
+{
+    public class CustomerResponseDto : BaseResponseDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public CustomerResponseDto(Customer customer)
+        {
+            Id = customer.Id;
+            Name = customer.Name;
+            Email = customer.Email;
+            Address = customer.Address;
+            CreatedAt = customer.CreatedAt;
+            UpdatedAt = customer.UpdatedAt;
+            DeletedAt = customer.DeletedAt;
+        }
+    }
+}
