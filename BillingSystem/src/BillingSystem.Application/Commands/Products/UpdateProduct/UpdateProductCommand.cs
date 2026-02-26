f6b6d4e7d69e7bee83257082b29764a7e3187f69
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 16:09:16 -0300

    FEAT: Adding update product command and handle

---
+++ BillingSystem/src/BillingSystem.Application/Commands/Products/UpdateProduct/UpdateProductCommand.cs
@@ -0,0 +1,17 @@
+﻿using BillingSystem.Application.DTOs.Requests.Products;
+using BillingSystem.Domain.Entities.Products;
+using MediatR;
+
+namespace BillingSystem.Application.Commands.Products.UpdateProduct
+{
+    public record UpdateProductCommand(
+        Guid Id,
+        ProductRequestDto RequestDto
+    ) : IRequest
+    {
+        public void UpdateDomain(Product product)
+        {
+            product.Update(RequestDto.Name);
+        }
+    }
+}
---
+++ BillingSystem/src/BillingSystem.Application/Commands/Products/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using BillingSystem.Common.Exceptions;
+using BillingSystem.Common.Exceptions.BaseExceptions;
+using BillingSystem.Domain.Interfaces.Data;
+using BillingSystem.Domain.Interfaces.Repositories.Products;
+using MediatR;
+
+namespace BillingSystem.Application.Commands.Products.UpdateProduct
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand>
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateProductCommandHandler(
+            IProductWriteRepository productWriteRepository,
+            IUnitOfWork unitOfWork
+        )
+        {
+            _productWriteRepository = productWriteRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productWriteRepository.GetByIdToUpdate(request.Id, cancellationToken)
+                ?? throw new NotFoundException(ResourceMessagesException.PRODUCT_NOT_FOUND);
+
+            request.UpdateDomain(product);
+
+            await _unitOfWork.CommitAsync(cancellationToken);
+        }
+    }
+}
