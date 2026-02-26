895d8ad9a2d7d18b57babce99ae7208715872efd
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 19:57:46 -0300

    FEAT: Adding query and handler to get billing by id

---
+++ BillingSystem/src/BillingSystem.Application/Queries/Billings/GetBillingById/GetBillingByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using BillingSystem.Application.DTOs.Responses.Billings;
+using MediatR;
+
+namespace BillingSystem.Application.Queries.Billings.GetBillingById
+{
+    public record GetBillingByIdQuery(Guid Id) : IRequest<BillingResponseDto>;
+}
---
+++ BillingSystem/src/BillingSystem.Application/Queries/Billings/GetBillingById/GetBillingByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using BillingSystem.Application.DTOs.Responses.Billings;
+using BillingSystem.Common.Exceptions;
+using BillingSystem.Common.Exceptions.BaseExceptions;
+using BillingSystem.Domain.Interfaces.Repositories.Billings;
+using MediatR;
+
+namespace BillingSystem.Application.Queries.Billings.GetBillingById
+{
+    public class GetBillingByIdQueryHandler : IRequestHandler<GetBillingByIdQuery, BillingResponseDto>
+    {
+        private readonly IBillingReadRepository _billingReadRepository;
+
+        public GetBillingByIdQueryHandler(IBillingReadRepository billingReadRepository)
+        {
+            _billingReadRepository = billingReadRepository;
+        }
+
+        public async Task<BillingResponseDto> Handle(GetBillingByIdQuery request, CancellationToken cancellationToken)
+        {
+            var billing = await _billingReadRepository.GetByIdAsync(request.Id, cancellationToken)
+                ?? throw new NotFoundException(ResourceMessagesException.BILLING_NOT_FOUND);
+
+            return new BillingResponseDto(billing);
+        }
+    }
+}
