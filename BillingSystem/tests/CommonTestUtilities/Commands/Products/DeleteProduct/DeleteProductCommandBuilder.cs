c5c2444bc2823dfd53a3b7b4482c79abc356b524
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 17:35:03 -0300

    FEAT: Creating DeleteProductCommandBuilder to handle delete product tests

---
+++ BillingSystem/tests/CommonTestUtilities/Commands/Products/DeleteProduct/DeleteProductCommandBuilder.cs
@@ -0,0 +1,16 @@
+﻿using BillingSystem.Application.Commands.Products.DeleteProduct;
+using Bogus;
+
+namespace CommonTestUtilities.Commands.Products.DeleteProduct
+{
+    public class DeleteProductCommandBuilder
+    {
+        public static DeleteProductCommand Build(Guid? id = null)
+        {
+            var fakerCommand = new Faker<DeleteProductCommand>()
+                .CustomInstantiator(f => new DeleteProductCommand(id ?? Guid.NewGuid()));
+
+            return fakerCommand.Generate();
+        }
+    }
+}
