d7ff54bdb102e004d99003f7eb0027ca5a93b7bc
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 01:27:24 -0300

    FEAT: Implementing validations on application layer for "fail-first" flow

---
+++ BillingSystem/src/BillingSystem.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,41 @@
+﻿using BillingSystem.Common.Exceptions.BaseExceptions;
+using FluentValidation;
+using MediatR;
+
+namespace BillingSystem.Application.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+            where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (!_validators.Any())
+                return await next(cancellationToken);
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResults = await Task.WhenAll(
+                _validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+            var failures = validationResults
+                .SelectMany(result => result.Errors)
+                .Where(failure => failure != null)
+                .ToList();
+
+            if (failures.Count != 0)
+            {
+                var errorMessages = failures.Select(f => f.ErrorMessage).Distinct().ToList();
+                throw new ErrorOnValidationException(errorMessages);
+            }
+
+            return await next(cancellationToken);
+        }
+    }
+}
