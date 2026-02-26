97d6ee1850b921b55e5175973b4986d162ced96d
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 11:32:22 -0300

    FEAT: Creating tests for register product validator

---
+++ BillingSystem/tests/Validators.Test/Products/RegisterProduct/RegisterProductValidatorTest.cs
@@ -0,0 +1,54 @@
+﻿using BillingSystem.Application.Commands.Products.RegisterProduct;
+using BillingSystem.Common.Exceptions;
+using CommonTestUtilities.Commands.Products.RegisterProduct;
+
+namespace Validators.Test.Products.RegisterProduct
+{
+    public class RegisterProductValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var validator = new RegisterProductCommandValidator();
+            var request = RegisterProductCommandBuilder.Build();
+            var result = validator.Validate(request);
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Error_Id_Empty()
+        {
+            var validator = new RegisterProductCommandValidator();
+            var request = RegisterProductCommandBuilder.Build();
+            request = request with { Id = Guid.Empty };
+
+            var result = validator.Validate(request);
+
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, e => e.ErrorMessage.Contains(ResourceMessagesException.ID_EMPTY));
+        }
+
+        [Fact]
+        public void Error_Name_Empty()
+        {
+            var validator = new RegisterProductCommandValidator();
+            var request = RegisterProductCommandBuilder.Build();
+            request = request with { Name = string.Empty };
+
+            var result = validator.Validate(request);
+
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, e => e.ErrorMessage.Contains(ResourceMessagesException.NAME_EMPTY));
+        }
+
+        [Fact]
+        public void Error_Max_Name_Length()
+        {
+            var validator = new RegisterProductCommandValidator();
+            var request = RegisterProductCommandBuilder.Build(101);
+            var result = validator.Validate(request);
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, e => e.ErrorMessage.Contains(string.Format(ResourceMessagesException.NAME_MAX_LENGTH, 100)));
+        }
+    }
+}
