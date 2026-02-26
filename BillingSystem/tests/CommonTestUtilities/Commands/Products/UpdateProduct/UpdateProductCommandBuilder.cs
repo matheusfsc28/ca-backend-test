f87da117dfeadd260f06dc49193f5ee2d711fe90
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 16:12:11 -0300

    FEAT: Adding builder to handle update product tests

---
+++ BillingSystem/tests/CommonTestUtilities/Commands/Products/UpdateProduct/UpdateProductCommandBuilder.cs
@@ -0,0 +1,20 @@
+﻿using BillingSystem.Application.Commands.Products.UpdateProduct;
+using BillingSystem.Application.DTOs.Requests.Products;
+using Bogus;
+
+namespace CommonTestUtilities.Commands.Products.UpdateProduct
+{
+    public class UpdateProductCommandBuilder
+    {
+        public static UpdateProductCommand Build(Guid? id = null, int nameLength = 10)
+        {
+            return new Faker<UpdateProductCommand>()
+                .CustomInstantiator(f => new UpdateProductCommand(
+                    id ?? Guid.NewGuid(),
+                    new ProductRequestDto(
+                        f.Commerce.ProductName().PadRight(nameLength, 'a')
+                    )
+                ));
+        }
+    }
+}
---
+++ BillingSystem/tests/CommonTestUtilities/Entities/ProductBuilder.cs
@@ -0,0 +1,18 @@
+﻿using BillingSystem.Domain.Entities.Products;
+using Bogus;
+
+namespace CommonTestUtilities.Entities
+{
+    public class ProductBuilder
+    {
+        public static Product Build()
+        {
+            var fakerProduct = new Faker<Product>()
+                .CustomInstantiator(f => 
+                    new Product(Guid.NewGuid(), f.Commerce.ProductName())
+                );
+
+            return fakerProduct.Generate();
+        }
+    }
+}
