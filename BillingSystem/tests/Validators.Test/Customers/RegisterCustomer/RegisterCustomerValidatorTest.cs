abafe51fe8d80178109bd9b8df208a508f328484
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 11:36:18 -0300

    FIX: Adding new tests for RegisterCustomerValidatorTest

--- BillingSystem/tests/Validators.Test/Customers/RegisterCustomer/RegisterCustomerValidatorTest.cs
+++ BillingSystem/tests/Validators.Test/Customers/RegisterCustomer/RegisterCustomerValidatorTest.cs
@@ -1,6 +1,6 @@
 ﻿using BillingSystem.Application.Commands.Customers.RegisterCustomer;
 using BillingSystem.Common.Exceptions;
-using CommonTestUtilities.Commands.Customers.NovaPasta;
+using CommonTestUtilities.Commands.Customers.RegisterCustomer;
 
 namespace Validators.Test.Customers.RegisterCustomer
 {
@@ -18,6 +18,19 @@ namespace Validators.Test.Customers.RegisterCustomer
         }
 
         [Fact]
+        public void Error_Id_Empty()
+        {
+            var validator = new RegisterCustomerValidator();
+            var request = RegisterCustomerCommandBuilder.Build();
+            request = request with { Id = Guid.Empty };
+
+            var result = validator.Validate(request);
+
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, e => e.ErrorMessage.Contains(ResourceMessagesException.ID_EMPTY));
+        }
+
+        [Fact]
         public void Error_Name_Empty()
         {
             var validator = new RegisterCustomerValidator();
@@ -28,6 +41,16 @@ namespace Validators.Test.Customers.RegisterCustomer
 
             Assert.False(result.IsValid);
             Assert.Contains(result.Errors, e => e.ErrorMessage.Contains(ResourceMessagesException.NAME_EMPTY));
+        }
+
+        [Fact]
+        public void Error_Name_Max_Length()
+        {
+            var validator = new RegisterCustomerValidator();
+            var request = RegisterCustomerCommandBuilder.Build(nameLength: 151);
+            var result = validator.Validate(request);
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, e => e.ErrorMessage.Contains(string.Format(ResourceMessagesException.NAME_MAX_LENGTH, 150)));
         }
 
         [Fact]
@@ -57,6 +80,18 @@ namespace Validators.Test.Customers.RegisterCustomer
         }
 
         [Fact]
+        public void Error_Email_Max_Length()
+        {
+            var validator = new RegisterCustomerValidator();
+            var request = RegisterCustomerCommandBuilder.Build(emailLenght: 151);
+
+            var result = validator.Validate(request);
+
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, e => e.ErrorMessage.Contains(string.Format(ResourceMessagesException.EMAIL_MAX_LENGTH, 150)));
+        }
+
+        [Fact]
         public void Error_Address_Empty()
         {
             var validator = new RegisterCustomerValidator();
@@ -67,6 +102,16 @@ namespace Validators.Test.Customers.RegisterCustomer
 
             Assert.False(result.IsValid);
             Assert.Contains(result.Errors, e => e.ErrorMessage.Contains(ResourceMessagesException.ADDRESS_EMPTY));
+        }
+
+        [Fact]
+        public void Error_Address_Max_Length()
+        {
+            var validator = new RegisterCustomerValidator();
+            var request = RegisterCustomerCommandBuilder.Build(addressLength: 251);
+            var result = validator.Validate(request);
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, e => e.ErrorMessage.Contains(string.Format(ResourceMessagesException.ADDRESS_MAX_LENGTH, 250)));
         }
     }
 }
