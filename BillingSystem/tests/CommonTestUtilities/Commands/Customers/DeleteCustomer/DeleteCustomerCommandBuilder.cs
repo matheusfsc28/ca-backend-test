5b69a216e3899635d3b7b747825427982e524e83
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 14:51:04 -0300

    FEAT: Adding builder for delete customer command tests

---
+++ BillingSystem/tests/CommonTestUtilities/Commands/Customers/DeleteCustomer/DeleteCustomerCommandBuilder.cs
@@ -0,0 +1,16 @@
+﻿using BillingSystem.Application.Commands.Customers.DeleteCustomer;
+using Bogus;
+
+namespace CommonTestUtilities.Commands.Customers.DeleteCustomer
+{
+    public class DeleteCustomerCommandBuilder
+    {
+        public static DeleteCustomerCommand Build(Guid? id = null)
+        {
+            return new Faker<DeleteCustomerCommand>()
+                .CustomInstantiator(f => new DeleteCustomerCommand(
+                    id ?? Guid.NewGuid()
+                ));
+        }
+    }
+}
