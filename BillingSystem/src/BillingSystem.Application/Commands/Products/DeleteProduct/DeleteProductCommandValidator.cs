dc460937208431b9739a8325b820ebc21ebfa6ce
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 17:34:36 -0300

    FEAT: Adding DeleteProductCommandValidator

---
+++ BillingSystem/src/BillingSystem.Application/Commands/Products/DeleteProduct/DeleteProductCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using BillingSystem.Common.Exceptions;
+using FluentValidation;
+
+namespace BillingSystem.Application.Commands.Products.DeleteProduct
+{
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator() 
+        {
+            RuleFor(request => request.Id)
+                .NotEmpty().WithMessage(ResourceMessagesException.ID_EMPTY);
+        }
+    }
+}
