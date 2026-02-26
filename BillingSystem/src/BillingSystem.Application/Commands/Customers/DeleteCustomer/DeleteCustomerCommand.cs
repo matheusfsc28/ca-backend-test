a5ed43d865ee6d624d002b748fbafa88fbc0a506
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 14:49:00 -0300

    FEAT: Adding command and handler to delete customer

---
+++ BillingSystem/src/BillingSystem.Application/Commands/Customers/DeleteCustomer/DeleteCustomerCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace BillingSystem.Application.Commands.Customers.DeleteCustomer
+{
+    public record DeleteCustomerCommand(Guid Id) : IRequest;
+}
---
+++ BillingSystem/src/BillingSystem.Application/Commands/Customers/DeleteCustomer/DeleteCustomerCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using BillingSystem.Common.Exceptions;
+using BillingSystem.Common.Exceptions.BaseExceptions;
+using BillingSystem.Domain.Interfaces.Data;
+using BillingSystem.Domain.Interfaces.Repositories.Customers;
+using MediatR;
+
+namespace BillingSystem.Application.Commands.Customers.DeleteCustomer
+{
+    public class DeleteCustomerCommandHandler : IRequestHandler<DeleteCustomerCommand>
+    {
+        private readonly ICustomerWriteRepository _customerWriteRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteCustomerCommandHandler(
+            ICustomerWriteRepository customerWriteRepository,
+            IUnitOfWork unitOfWork
+        )
+        {
+            _customerWriteRepository = customerWriteRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var customer = await _customerWriteRepository.GetByIdToUpdate(request.Id, cancellationToken)
+                ?? throw new NotFoundException(ResourceMessagesException.CUSTOMER_NOT_FOUND);
+
+            customer.Delete();
+
+            await _unitOfWork.CommitAsync(cancellationToken);
+        }
+    }
+}
