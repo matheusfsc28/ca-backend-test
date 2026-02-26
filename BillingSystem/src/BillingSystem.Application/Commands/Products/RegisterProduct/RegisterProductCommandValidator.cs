c23da7984ecee5a8791168b4dc8b3abf36500e90
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-26 11:29:55 -0300

    FEAT: Adding register product command validator

---
+++ BillingSystem/src/BillingSystem.Application/Commands/Products/RegisterProduct/RegisterProductCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using BillingSystem.Common.Exceptions;
+using FluentValidation;
+
+namespace BillingSystem.Application.Commands.Products.RegisterProduct
+{
+    public class RegisterProductCommandValidator : AbstractValidator<RegisterProductCommand>
+    {
+        public RegisterProductCommandValidator() 
+        {
+            RuleFor(request => request.Id)
+                .NotEmpty().WithMessage(ResourceMessagesException.ID_EMPTY);
+
+            RuleFor(request => request.Name)
+                .NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY)
+                .MaximumLength(100).WithMessage(string.Format(ResourceMessagesException.NAME_MAX_LENGTH, 100));
+        }
+    }
+}
