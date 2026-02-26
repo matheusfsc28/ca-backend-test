a2ad6dbac2899dac5894b4cf20e89350b4322638
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-25 22:12:49 -0300

    FEAT: Creating external billing API interface

---
+++ BillingSystem/src/BillingSystem.Application/Abstractions/ExternalBillingService/IExternalBillingService.cs
@@ -0,0 +1,9 @@
+﻿using BillingSystem.Application.DTOs.Responses.ExternalBillingService;
+
+namespace BillingSystem.Application.Abstractions.ExternalBillingService
+{
+    public interface IExternalBillingService
+    {
+        Task<IEnumerable<ExternalBillingResponseDto>> GetBillingsAsync(CancellationToken cancellationToken = default);
+    }
+}
