d4aa0ea5777957f6108ba495bd8bcba30bc36203
Matheus Felipe <matheus.felipescruz@gmail.com>
2026-02-26 23:50:04 -0300

    FEAT: Adding API layer

---
+++ BillingSystem/src/BillingSystem.Api/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Billing System API",
+        Version = "v1",
+        Description = "API para gestão de clientes, produtos e sincronização de notas fiscais.",
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
