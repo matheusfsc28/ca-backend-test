25b87e7246c66324ac68d42a9d8dfb5478fffc9d
Matheus Felipe <matheus.felipescruz@gmail.com>
2026-02-27 00:50:33 -0300

    FEAT: adding BillingSystemV1BaseController

---
+++ BillingSystem/src/BillingSystem.Api/Controllers/V1/Base/BillingSystemV1BaseController.cs
@@ -0,0 +1,19 @@
+﻿using BillingSystem.Application.DTOs.Responses.Base;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillingSystem.Api.Controllers.V1.Base
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ProducesResponseType(typeof(ErrorResponseDto), StatusCodes.Status500InternalServerError)]
+    public class BillingSystemV1BaseController : ControllerBase
+    {
+        protected readonly IMediator _mediator;
+        public BillingSystemV1BaseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
