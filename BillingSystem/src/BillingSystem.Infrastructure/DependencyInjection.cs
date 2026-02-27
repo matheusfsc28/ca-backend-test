08a53cf273f45343ec5cc590959d2f9d3af258d5
Matheus Felipe <matheus.felipescruz@gmail.com>
2026-02-26 22:35:22 -0300

    FEAT: Configuring client for ExternalBillingService on depedency injection

--- BillingSystem/src/BillingSystem.Infrastructure/DependencyInjection.cs
+++ BillingSystem/src/BillingSystem.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using BillingSystem.Infrastructure.Data;
+﻿using BillingSystem.Application.Abstractions.ExternalBillingService;
+using BillingSystem.Infrastructure.Data;
+using BillingSystem.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,9 +11,23 @@ namespace BillingSystem.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            AddExternalServices(services, configuration);
+
             AddDbContext(services, configuration);
 
             return services;
+        }
+
+        private static void AddExternalServices(IServiceCollection services, IConfiguration configuration)
+        {
+            var baseUrl = configuration.GetValue<string>("ExternalBillingServiceSettings:BaseUrl");
+
+            services.AddHttpClient<IExternalBillingService, ExternalBillingService>(client =>
+            {
+                client.BaseAddress = new Uri(baseUrl!);
+                client.Timeout = TimeSpan.FromSeconds(30);
+            })
+            .AddStandardResilienceHandler();
         }
 
         private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
