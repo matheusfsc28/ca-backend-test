ff16e1dc793334e949fa37b4edd8849a828b43c1
Matheus Felipe <matheus.felipescruz@gmail.com>
2026-02-27 00:32:17 -0300

    FEAT: Creating response to use on exception filter

---
+++ BillingSystem/src/BillingSystem.Application/DTOs/Responses/Base/ErrorResponseDto.cs
@@ -0,0 +1,17 @@
+﻿namespace BillingSystem.Application.DTOs.Responses.Base
+{
+    public class ErrorResponseDto
+    {
+        public IEnumerable<string> Errors { get; set; }
+
+        public ErrorResponseDto(IEnumerable<string> errors)
+        {
+            Errors = errors;
+        }
+
+        public ErrorResponseDto(string error)
+        {
+            Errors = [error];
+        }
+    }
+}
---
+++ BillingSystem/src/BillingSystem.Application/DTOs/Responses/Billings/ErrorOnSyncBillingResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BillingSystem.Application.DTOs.Responses.Billings
+{
+    public class ErrorOnSyncBillingResponseDto
+    {
+        public int SyncedBillingsCount { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        public ErrorOnSyncBillingResponseDto(int syncedBillingsCount, IEnumerable<string> errors)
+        {
+            SyncedBillingsCount = syncedBillingsCount;
+            Errors = errors;
+        }
+    }
+}
