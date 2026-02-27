763a5eb102625cd2aaa0fcaec170361d4396e3f8
Matheus Felipe <matheus.felipescruz@gmail.com>
2026-02-26 22:33:02 -0300

    FEAT: Creating new excpetion for external services

---
+++ BillingSystem/src/BillingSystem.Common/Exceptions/BaseExceptions/ExternalServiceException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace BillingSystem.Common.Exceptions.BaseExceptions
+{
+    public class ExternalServiceException : BillingSystemException
+    {
+        public ExternalServiceException(string message) : base(message) { }
+
+        public override IEnumerable<string> GetErrorMessages() => [Message];
+
+        public override HttpStatusCode GetStatusCode() => HttpStatusCode.FailedDependency;
+    }
+}
