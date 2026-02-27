b091dfc6c16a9ec18cd495607a276494f74d2209
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-27 12:41:45 -0300

    FEAT: Adding BillingSystemClassFixture to handle with api calls

---
+++ BillingSystem/tests/WebApi.Test/BillingSystemClassFixture.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace WebApi.Test
+{
+    public class BillingSystemClassFixture : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _httpClient;
+        
+        public BillingSystemClassFixture(CustomWebApplicationFactory factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+
+        protected async Task<HttpResponseMessage> DoPost(string endpoint, object request, string culture = "en")
+        {
+            ChangeRequestCulture(culture);
+
+            return await _httpClient.PostAsJsonAsync(endpoint, request);
+        }
+
+        protected async Task<HttpResponseMessage> DoGet(string endpoint, string culture = "en")
+        {
+            ChangeRequestCulture(culture);
+            return await _httpClient.GetAsync(endpoint);
+        }
+
+        protected async Task<HttpResponseMessage> DoPut(string endpoint, object request,  string culture = "en")
+        {
+            ChangeRequestCulture(culture);
+            return await _httpClient.PutAsJsonAsync(endpoint, request);
+        }
+
+        protected async Task<HttpResponseMessage> DoDelete(string endpoint, string culture = "en")
+        {
+            ChangeRequestCulture(culture);
+            return await _httpClient.DeleteAsync(endpoint);
+        }
+
+        private void ChangeRequestCulture(string culture)
+        {
+            _httpClient.DefaultRequestHeaders.AcceptLanguage.Clear();
+            _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(culture));
+        }
+    }
+}
