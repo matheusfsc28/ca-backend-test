bdc35eb8427db55483513136cf7e475e57063884
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-27 19:03:48 -0300

    FIX: Adjusting SnakeCaseParameterModelConvention

--- BillingSystem/src/BillingSystem.Api/Configurations/ModelBinding/SnakeCaseParameterModelConvention.cs
+++ BillingSystem/src/BillingSystem.Api/Configurations/ModelBinding/SnakeCaseParameterModelConvention.cs
@@ -9,6 +9,13 @@ namespace BillingSystem.Api.Configurations.ModelBinding
         {
             if (parameter.BindingInfo?.BindingSource == Microsoft.AspNetCore.Mvc.ModelBinding.BindingSource.Query)
             {
+                var typeNamespace = parameter.ParameterType.Namespace;
+
+                if (typeNamespace != null && typeNamespace.StartsWith("BillingSystem"))
+                {
+                    return;
+                }
+
                 if (string.IsNullOrEmpty(parameter.BindingInfo.BinderModelName))
                 {
                     parameter.BindingInfo.BinderModelName = JsonNamingPolicy.SnakeCaseLower.ConvertName(parameter.ParameterName);
