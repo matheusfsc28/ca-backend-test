cbf85fed68254db1c51e54a5eda479d826b55bb7
matheusfsc28 <matheus.felipescruz@gmail.com>
2026-02-27 19:05:03 -0300

    FIX: Adjusting PaginationRequestDto to load BaseRequestDto on requests

--- BillingSystem/src/BillingSystem.Application/DTOs/Requests/Base/PaginationRequestDto.cs
+++ BillingSystem/src/BillingSystem.Application/DTOs/Requests/Base/PaginationRequestDto.cs
@@ -1,9 +1,10 @@
 ﻿namespace BillingSystem.Application.DTOs.Requests.Base
 {
-    public class PaginationRequestDto<T> where T : BaseRequestDto
+    public class PaginationRequestDto<T> where T : BaseRequestDto, new()
     {
         public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 10;
-        public T? Filters { get; set; } = null;
+        public T Filters { get; set; } = new T();
+
     }
 }
